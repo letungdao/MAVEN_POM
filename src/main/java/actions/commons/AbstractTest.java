ba49e4f51345fc2a0ae50380cf8305f79b99fa68
letungdao <daoletung.dn@gmail.com>
2024-02-27 18:35:36 +0700

    update

--- src/main/java/actions/commons/AbstractTest.java
+++ src/main/java/actions/commons/AbstractTest.java
@@ -136,18 +136,26 @@ public class AbstractTest {
 		// copying File path to Clipboard
 		StringSelection str = new StringSelection(fileUpload);
 		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(str, null);
+		
+		// for pressing and releasing Enter
+		rb.delay(400);
+		rb.keyPress(KeyEvent.VK_ENTER);
+		rb.keyRelease(KeyEvent.VK_ENTER);
 
 		// press Contol+V for pasting
 		rb.keyPress(KeyEvent.VK_CONTROL);
 		rb.keyPress(KeyEvent.VK_V);
-
+		rb.delay(400);
+		
 		// release Contol+V for pasting
 		rb.keyRelease(KeyEvent.VK_CONTROL);
 		rb.keyRelease(KeyEvent.VK_V);
+		rb.delay(400);
 
 		// for pressing and releasing Enter
 		rb.keyPress(KeyEvent.VK_ENTER);
 		rb.keyRelease(KeyEvent.VK_ENTER);
+		rb.delay(400);
 	}
 
 }
